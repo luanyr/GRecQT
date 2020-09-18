3735bcda15b9e9a0199b31b0e8fe8760aaf56ee2
virtual pc <599695150@qq.com>
2020-09-18 08:59:35 +0800

    <style>:daily update

--- form_client.cpp
+++ form_client.cpp
@@ -17,23 +17,22 @@ Form_client::~Form_client()
 
 void Form_client::Startmenu_Init()
 {
-
-    sm = new startmenu(this);
-    rcdm = new recordmenu(this);
-    rpm = new repmenu(this);
-    fm = new filmenu(this);
-    mgm = new managemenu(this);
-    um = new usermenu(this);
-    tm = new tunnelmenu(this);
-    spm = new specfymenu(this);
-    TBmenu = new QToolBox(this);
+    sm    = new startmenu(this);
+    rcdm  = new recordmenu(this);
+    rpm   = new repmenu(this);
+    fm    = new filmenu(this);
+    mgm   = new managemenu(this);
+    um    = new usermenu(this);
+    tm    = new tunnelmenu(this);
+    spm   = new specfymenu(this);
+    TBmenu= new QToolBox(this);
     TBmenu->resize(1000, 350);
-    TBmenu->addItem(sm, tr("开始"));
-    TBmenu->addItem(rcdm, tr("记录"));
-    TBmenu->addItem(rpm, tr("回放"));
-    TBmenu->addItem(fm, tr("筛选"));
-    TBmenu->addItem(mgm, tr("管理"));
-    TBmenu->addItem(um, tr("用户"));
-    TBmenu->addItem(tm, tr("通道"));
-    TBmenu->addItem(spm, tr("特殊"));
+    TBmenu->addItem(  sm,     tr("开始"));
+    TBmenu->addItem(rcdm,     tr("记录"));
+    TBmenu->addItem( rpm,     tr("回放"));
+    TBmenu->addItem(  fm,     tr("筛选"));
+    TBmenu->addItem( mgm,     tr("管理"));
+    TBmenu->addItem(  um,     tr("用户"));
+    TBmenu->addItem(  tm,     tr("通道"));
+    TBmenu->addItem( spm,     tr("特殊"));
 }
